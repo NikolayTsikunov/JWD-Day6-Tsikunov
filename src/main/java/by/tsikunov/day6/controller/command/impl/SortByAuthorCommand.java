8192e5f32b16dfa8228ea70f588e0ffdce487e46
NikolayTsikunov <nikolay.tsikunov@mail.ru>
2020-07-20 06:21:06 +0300

    Add sort commands

--- src/main/java/by/tsikunov/day6/controller/command/impl/SortByAuthorCommand.java
+++ src/main/java/by/tsikunov/day6/controller/command/impl/SortByAuthorCommand.java
@@ -1,12 +1,22 @@
 package by.tsikunov.day6.controller.command.impl;
 
 import by.tsikunov.day6.controller.command.ExecutedCommand;
+import by.tsikunov.day6.model.service.LibraryService;
+import by.tsikunov.day6.model.service.impl.LibraryServiceImpl;
+import by.tsikunov.day6.model.service.response.Response;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SortByAuthorCommand implements ExecutedCommand {
     @Override
     public Map<String, String> execute(Map<String, String> parameters) {
-        return null;
+        LibraryService service = new LibraryServiceImpl();
+        Map<String, String> result = new HashMap<>();
+
+        Response response = service.sortByAuthor();
+        result.put("Message", "Sorted list by author");
+        result.put("Storage", response.getResultBooks().toString());
+        return result;
     }
 }
--- src/main/java/by/tsikunov/day6/controller/command/impl/SortByPublicationYearCommand.java
+++ src/main/java/by/tsikunov/day6/controller/command/impl/SortByPublicationYearCommand.java
@@ -1,12 +1,22 @@
 package by.tsikunov.day6.controller.command.impl;
 
 import by.tsikunov.day6.controller.command.ExecutedCommand;
+import by.tsikunov.day6.model.service.LibraryService;
+import by.tsikunov.day6.model.service.impl.LibraryServiceImpl;
+import by.tsikunov.day6.model.service.response.Response;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class SortByPublicationYearCommand implements ExecutedCommand {
     @Override
     public Map<String, String> execute(Map<String, String> parameters) {
-        return null;
+        LibraryService service = new LibraryServiceImpl();
+        Map<String, String> result = new HashMap<>();
+
+        Response response = service.sortByPublicationYear();
+        result.put("Message", "Sorted list by publication year");
+        result.put("Storage", response.getResultBooks().toString());
+        return result;
     }
 }
